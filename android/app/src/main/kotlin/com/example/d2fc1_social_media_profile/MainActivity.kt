b35a799892390fd40a3c73d74b0f69254e992927
ProtoManGG <pranav.taneja36@gmail.com>
2021-08-14 16:24:46 +0530

    Inital setup

---
+++ android/app/src/main/kotlin/com/example/d2fc1_social_media_profile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.d2fc1_social_media_profile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:d2fc1_social_media_profile/profile/profile_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ProfileApp());
+}
+
+class ProfileApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProfileScreen(),
+    );
+  }
+}
